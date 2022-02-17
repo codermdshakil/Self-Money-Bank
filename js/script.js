cf1b67a181be828459da64b6058f3682f16feef9
farabikhan0412 <ahmedshakil0512@gmail.com>
2022-02-17 19:04:28 +0600

    js file add

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*  get input elements */ 
+const incomeInput = document.getElementById('income-amount');
+const foodInput = document.getElementById('food-input');
+const rentInput = document.getElementById('rent-input');
+const clothInput = document.getElementById('cloth-input');
+const savePercentage = document.getElementById('percentage-input');
+
+/* get Elements of html  */
+const expensesTotal = document.getElementById('expenses-total');
+const balanceTotal = document.getElementById('total-balance');
+const saveAmount = document.getElementById('save-amount');
+const remainingBalance = document.getElementById('remaining-blance');
+
+
+/*  get error massages  */
+const firstError = document.getElementById('first-error');
+const secoundError = document.getElementById('secound-error');
+const thirdError = document.getElementById('third-error');
+
+
